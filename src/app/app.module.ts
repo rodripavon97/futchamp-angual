3621a19cd86c89e193c79a68237c45df9e90c401
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-11-14 20:31:13 -0300

    creando la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavbarComponent, FooterComponent, FormularioComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
